1b4c77eb4cb96409774cefa98387b18344ae85dd
agar91 <agnieszkarapa@gmail.com>
2021-03-26 12:52:34 +0100

    first commit

---
+++ server-product-management/src/test/java/com/raga/serverproductmanagement/ServerProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raga.serverproductmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
