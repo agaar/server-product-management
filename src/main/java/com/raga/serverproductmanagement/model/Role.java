6e6ac35ab13b9f5a6ecb1941d5fa0823f315d272
agar91 <agnieszkarapa@gmail.com>
2021-03-26 13:06:35 +0100

    user model created

---
+++ src/main/java/com/raga/serverproductmanagement/model/Role.java
@@ -0,0 +1,6 @@
+package com.raga.serverproductmanagement.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/raga/serverproductmanagement/model/User.java
+++ src/main/java/com/raga/serverproductmanagement/model/User.java
@@ -1,7 +1,10 @@
 package com.raga.serverproductmanagement.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data //auto getters, setters, equals, hashcode
 @Entity
 @Table(name="user")
 public class User {
@@ -9,4 +12,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="role")
+    private Role role;
 }
