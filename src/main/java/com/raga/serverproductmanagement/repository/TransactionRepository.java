a22d927bb128b003a87115bc2cec9e97b6c63c2d
agar91 <agnieszkarapa@gmail.com>
2021-03-27 09:58:48 +0100

    product, transaction repository impl

--- src/main/java/com/raga/serverproductmanagement/repository/TransactionRepository.java
+++ src/main/java/com/raga/serverproductmanagement/repository/TransactionRepository.java
@@ -1,12 +1,12 @@
 package com.raga.serverproductmanagement.repository;
 
+import com.raga.serverproductmanagement.model.Transaction;
 import com.raga.serverproductmanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository <User, Long> { //model class, id class
+public interface TransactionRepository extends JpaRepository <Transaction, Long> { //model class, id class
 
-    //findBy + fieldName
-    Optional<User> findByUsername (String username);
+
 }
